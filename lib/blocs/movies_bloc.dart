1cef73e67fd08384153acb044e4c7a8f8cd0e21e
David Cruz Anaya <me@davidcruz.co.uk>
2019-11-16 14:24:09 +0000

    Update movies_bloc.dart

---
+++ lib/blocs/movies_bloc.dart
@@ -0,0 +1,21 @@
+import 'package:rxdart/rxdart.dart';
+import 'package:rxdart/subjects.dart';
+import 'package:themoviedb_flutter_bloc/models/movies/movie_item.dart';
+import 'package:themoviedb_flutter_bloc/repositories/movie_repository.dart';
+
+class MoviesBloc {
+  final _movieRepository = MovieRepository();
+  final _movies = PublishSubject();
+
+  Observable<MovieItem> get popularMovies => _movies.stream;
+  
+  getPopularMovies() async {
+    MovieItem movieItem = await _movieRepository.getPopularMovies;
+    _movies.sink.add(movieItem);
+  }
+
+  dispose() {
+    _movies.close();
+  }
+  
+}
